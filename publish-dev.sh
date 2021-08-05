97f37fd6e52b9169f71c67bd8dafcb8c25d8b37c
Slava Simonov <b7he6tkwhyhcp4orajfdi24zggfncrfpcp7a456bpoh36yq4wf7q>
2021-08-05 21:05:19 +0400

    Add publish.sh files

---
+++ publish-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Publish package version with a version (picks up from package.json so please update it before running this).
+#
+# Copyright 2021 Crowdlinker
+#
+# You should have received a copy of the MIT along with this program.
+# If not, see <https://opensource.org/licenses/MIT>.
+
+npm run build
+cp README.md ./dist
+cp LICENSE ./dist
+cp package.json ./dist
+cd ./dist
+npm publish --access public --tag dev
